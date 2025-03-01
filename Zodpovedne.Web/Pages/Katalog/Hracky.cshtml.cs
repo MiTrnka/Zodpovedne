05f723c946a6750c301c35f20b1a0b2937d73e21
Marketa Trnkova <Maky.trnkova@gmail.com>
2025-03-01 19:31:15 +0100

    Úprava katalogu - přidání banneru a mockup dat

--- Zodpovedne.Web/Pages/Katalog/Hracky.cshtml.cs
+++ Zodpovedne.Web/Pages/Katalog/Hracky.cshtml.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Zodpovedne.Logging;
+using Zodpovedne.Web.Models.Base;
 
 namespace Zodpovedne.Web.Pages.Katalog
 {
-    public class HrackyModel : PageModel
+    public class HrackyModel : BasePageModel
     {
+        public HrackyModel(IHttpClientFactory clientFactory, IConfiguration configuration, FileLogger logger) : base(clientFactory, configuration, logger)
+        {
+        }
         public void OnGet()
         {
         }
