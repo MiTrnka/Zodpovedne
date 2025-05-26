072d49c5ca123a7d3c28bc39783c3dfd4cfd0eab
Michal Trnka <MiTrnka@gmail.com>
2025-05-26 17:04:45 +0200

    nove sql scripty

--- Zodpovedne.Data/SQL Scripts/dotazy.sql
+++ Zodpovedne.Data/SQL Scripts/dotazy.sql
@@ -15,9 +15,28 @@ FROM 	"CommentLikes" inner join
 		"Comments" on "Comments"."Id" = "CommentLikes"."CommentId"
 WHERE	"CommentLikes"."UserId" not in ('6f925769-14f0-41c8-b79f-135db37fc466','5cd9ed7f-9bbd-484e-95dd-5d96e6305cf5','a65603cb-089f-441a-8c39-6f2b1d92d804')
 
--- Zprávy
+-- Poslední zprávy
 SELECT 	sender."Nickname" odesilatel, recipient."Nickname" prijemce, "SentAt" odeslano, "ReadAt" precteno, "Content"
 FROM 	"Messages" inner join
 		"Users" sender on sender."Id" = "Messages"."SenderUserId" inner join
 		"Users" recipient on recipient."Id" = "Messages"."RecipientUserId"
 ORDER BY "Messages"."SentAt" DESC
+
+-- Poslední komentáře
+SELECT u."Nickname" ,c."ParentCommentId", c."Content", c."CreatedAt", c."UpdatedAt", c."Type"
+FROM "Comments" c inner join "Users" u on c."UserId" = u."Id"
+ORDER BY "CreatedAt" DESC
+
+-- Poslední lajky diskuzí
+SELECT 	u."Nickname", d."Title", dl."CreatedAt"
+FROM 	"DiscussionLikes" dl inner join 
+		"Users" u on dl."UserId" = u."Id" inner join
+		"Discussions" d on dl."DiscussionId" = d."Id"
+ORDER BY dl."CreatedAt" DESC
+
+-- Poslední lajky komentářů
+SELECT 	u."Nickname", c."Content", cl."CreatedAt"
+FROM 	"CommentLikes" cl inner join 
+		"Users" u on cl."UserId" = u."Id" inner join
+		"Comments" c on cl."CommentId" = c."Id"
+ORDER BY cl."CreatedAt" DESC
