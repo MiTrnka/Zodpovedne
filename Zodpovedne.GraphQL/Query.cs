3fc411befc8a2632bc3716b366516fd61b4f8a05
Michal Trnka <MiTrnka@gmail.com>
2025-08-09 08:03:50 +0200

    Pridana maui aplikace a graphql

---
+++ Zodpovedne.GraphQL/Query.cs
@@ -0,0 +1,8 @@
+﻿namespace Zodpovedne.GraphQL;
+
+public class Query
+{
+    // Toto je jednoduchá metoda, která se v GraphQL schématu objeví
+    // jako dotaz (query) s názvem "hello".
+    public string Hello() => "GraphQL API běží!";
+}
---
+++ Zodpovedne.MAUIApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Zodpovedne.MAUIApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
