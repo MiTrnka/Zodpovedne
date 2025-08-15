1fb841e4c60d03f3fe004e57781e462b20e14a01
Michal Trnka <MiTrnka@gmail.com>
2025-08-15 10:23:45 +0200

    priprava na novou verzi

--- Zodpovedne.MAUIApp/Permissions/PostNotificationsPermission.cs
+++ Zodpovedne.MAUIApp/Permissions/PostNotificationsPermission.cs
@@ -7,6 +7,10 @@ namespace Zodpovedne.MAUIApp.Permissions;
 /// <summary>
 /// Vlastní definice oprávnění pro zasílání notifikací na Androidu 13 a vyšším.
 /// .NET MAUI vyžaduje tuto třídu, aby věděl, jak o systémové oprávnění požádat.
+/// Tato třída definuje vlastní oprávnění pro zasílání notifikací, které je vyžadováno na Androidu 13 (API 33) a vyšším.
+/// Funguje jako nezbytný most mezi deklarací oprávnění v AndroidManifest.xml a C# kódem aplikace.
+/// Umožňuje multiplatformní metodě Permissions.RequestAsync<T>() zjistit, jaké konkrétní nativní
+/// oprávnění má v systému Android vyžádat, a následně zobrazit uživateli příslušný dialog.
 /// </summary>
 public class PostNotificationsPermission : BasePlatformPermission
 {
